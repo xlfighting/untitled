1039cab1b1ae1929f329ae2f6838d897412c7f79
Layne <3110541445@qq.com>
2024-07-29 08:51:56 +0800

    hotfix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("Hello GIT2");
+        System.out.println("Hello GIT3");
     }
 }
